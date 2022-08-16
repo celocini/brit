e482f3c36fe3ca7fafdcb58c5cdd48aa9853f29e
celocini <73112259+celocini@users.noreply.github.com>
2022-08-16 14:18:24 -0300

    expande.js

---
+++ js/expande.js
@@ -0,0 +1,9 @@
+function expande_end() {
+    document.getElementById("fieldend").style.display="block"
+    document.getElementById("imgexpandirend").style.visibility="hidden"
+}
+
+function expande_rep() {
+    document.getElementById("fieldrep").style.display="block"
+    document.getElementById("imgexpandirrep").style.visibility="hidden"
+}
